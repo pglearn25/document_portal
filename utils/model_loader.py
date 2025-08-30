f00ba08a2991a77c7c1c4dd0816d018fb45a6047
Priya Gupta <priya@Priya-ka-MacBook-Air.local>
2025-08-30 09:38:56 +0530

    model load

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -8,7 +8,7 @@ from langchain_groq import ChatGroq
 from logger import GLOBAL_LOGGER as log
 from exception.custom_exception import DocumentPortalException
 
-
+# for loading model in ECS or local
 class ApiKeyManager:
     REQUIRED_KEYS = ["GROQ_API_KEY", "GOOGLE_API_KEY"]
 
